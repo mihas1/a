df0de910aae3f95d80eefcac8d3dff3ae2233b2b
mikhail.khabarov <mikhail.khabarov@x2sy.com>
2019-10-01 16:06:56 +0300

    Updates

---
+++ precache-manifest.3a9a164f5d99786d84bec1d136f14efd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5255dad7fca4c2d524c1",
+    "url": "/static/js/main.5255dad7.chunk.js"
+  },
+  {
+    "revision": "366377536349553bbd27",
+    "url": "/static/js/1.36637753.chunk.js"
+  },
+  {
+    "revision": "5255dad7fca4c2d524c1",
+    "url": "/static/css/main.fadd1f84.chunk.css"
+  },
+  {
+    "revision": "25e0a8c0daf85a619e8d5f716b6d3e58",
+    "url": "/index.html"
+  }
+];
